c5c1c4021177e4fae15939939dab2d756ccbf474
fordevoted <210509fssh@gmail.com>
2020-12-14 17:50:36 +0800

    update feature_extracetion.py; add README.md for feature extraction

---
+++ statistics_model/data_preprocessing/README.md
@@ -0,0 +1,24 @@
+# Data Science Final Proj. Feature Extraction 
+
+## Data
+-  train 1 complement
+-  train 1 trend
+-  train 1 seasonality
+-  train 1 residual
+-  train 2 complement
+-  train 2 trend
+-  train 2 seasonality
+-  train 2 residual
+
+## Seasonal Decompose
+- Using **additive** model: which means 
+ ` result = Trend + Seasonal + Residual`, and the period is set to `7` temporarily 
+
+## Complement Missing Value
+- using KNN to impute missing value temporarily
+- time series will be remove if missing value is higher than **50%** 
+
+## Usage
+download data from [here](https://drive.google.com/drive/folders/1FuRki8KuII1hj-868KJeClXOgTqKcYVj?usp=sharing)  
+
+ 
--- statistics_model/data_preprocessing/feature_extracetion.py
+++ statistics_model/data_preprocessing/feature_extracetion.py
@@ -4,59 +4,76 @@ import numpy as np
 from sklearn.impute import KNNImputer
 from statsmodels.tsa.seasonal import seasonal_decompose
 from tsfresh.feature_extraction import ComprehensiveFCParameters
+from tsfresh.feature_extraction import EfficientFCParameters
 
 # Data
-file_name = 'train_1'
+file_name = 'train_2'
 data_path = r'C:\Users\USER\Desktop\DS\final\\' + file_name + ".csv"
 train = pd.read_csv(data_path)
 page = train['Page']
 del train['Page']
-mode = 'row_zero'
-# train = train.head(10)
+mode = 'row_KNN'
 size = train.shape[0]
+# train = train.head(10)
 
+complement_finish = True
+feature_finish = False
 if __name__ == '__main__':
     # complement missing value
-    for index, row in train.iterrows():
-        nan_num = row.isna().sum()
-        if nan_num / row.size > 0.5:
-            train = train.drop(index)
-        elif nan_num != 0:
-            if mode == 'row_KNN':
-                imputer = KNNImputer(n_neighbors=15, weights='uniform', metric='nan_euclidean')
-                data = row.array
-                data = np.expand_dims(data, axis=1)
-                imputer.fit(data)
-                data = imputer.transform(data)
-                data = data.squeeze()
-                row = pd.Series(data, index=row.index)
-            elif mode == 'row_median':
-                row = row.fillna(np.median(row))
-            elif mode == 'row_mean':
-                row = row.fillna(np.mean(row))
-            elif mode == 'row_min':
-                row = row.fillna(np.min(row))
-            elif mode == 'row_max':
-                row = row.fillna(np.max(row))
-            elif mode == 'row_zero':
-                row = row.fillna(0)
-            train.loc[index] = row
-        print("processing: {index}/{total} ".format(index=index, total=size))
-    train.to_csv('./save_csv/' + file_name + "_complement.csv")
-
+    if not complement_finish:
+        for index, row in train.iterrows():
+            nan_num = row.isna().sum()
+            if nan_num / row.size > 0.5:
+                train = train.drop(index)
+                page.drop(index)
+            elif nan_num != 0:
+                if mode == 'row_KNN':
+                    imputer = KNNImputer(n_neighbors=15, weights='uniform', metric='nan_euclidean')
+                    data = row.array
+                    data = np.expand_dims(data, axis=1)
+                    imputer.fit(data)
+                    data = imputer.transform(data)
+                    data = data.squeeze()
+                    row = pd.Series(data, index=row.index)
+                elif mode == 'row_median':
+                    row = row.fillna(np.median(row))
+                elif mode == 'row_mean':
+                    row = row.fillna(np.mean(row))
+                elif mode == 'row_min':
+                    row = row.fillna(np.min(row))
+                elif mode == 'row_max':
+                    row = row.fillna(np.max(row))
+                elif mode == 'row_zero':
+                    row = row.fillna(0)
+                train.loc[index] = row
+            print("processing: {index}/{total} ".format(index=index, total=size))
+        train['Page'] = page
+        train.to_csv('./save_csv/' + file_name + "_complement.csv")
+    else:
+        train = pd.read_csv('./save_csv/' + file_name + "_complement.csv")
+        del train['Page']
     # tsfresh feature extraction -> PCA -> clustering (k-means)
     # train["Page"] = page.head(10)
-    train["Page"] = page
-    y = train[train.columns[-2]]
-    y.index = train['Page']
-    settings = ComprehensiveFCParameters()
-    extracted_feature = tsf.extract_relevant_features(y=y, timeseries_container=train, default_fc_parameters=settings,
-                                                      column_id="Page")
-    print(extracted_feature)
-    extracted_feature.to_csv('./save_csv/' + file_name + "_feature.csv")
+    if not feature_finish:
+        feature_result = None
+        for index, row in train.iterrows():
+            temp = pd.DataFrame(row).T
+            del temp['Unnamed: 0']
+            temp = pd.DataFrame({'values': temp.T.values.squeeze(), 'times': temp.columns})
+            temp['id'] = (page.loc[index])[0]
+            settings = EfficientFCParameters()
+            extracted_feature = tsf.extract_features(timeseries_container=temp, default_fc_parameters=settings, column_id="id", column_sort='times')
+            extracted_feature['Page'] = page.loc[index]
+            if feature_result is None:
+                feature_result = extracted_feature
+            else:
+                feature_result = feature_result.append(extracted_feature)
+        # print(extracted_feature)
+        feature_result.to_csv('./save_csv/' + file_name + "_feature.csv", index=False)
 
     # cycle/seasonality, trend  extraction
-    del train['Page']
+    if not complement_finish:
+        del train['Page']
     trend = []
     seasonal = []
     residual = []
@@ -82,6 +99,6 @@ if __name__ == '__main__':
     residual.columns = train.columns
     residual['Page'] = page
 
-    trend.to_csv('./save_csv/' + file_name + "_trend.csv")
-    seasonal.to_csv('./save_csv/' + file_name + "_seasonal.csv")
-    residual.to_csv('./save_csv/' + file_name + "_residual.csv")
+    trend.to_csv('./save_csv/' + file_name + "_trend.csv", index=False)
+    seasonal.to_csv('./save_csv/' + file_name + "_seasonal.csv", index=False)
+    residual.to_csv('./save_csv/' + file_name + "_residual.csv", index=False)
